3755d46f3b7a5d658f7b6ab1d2c9299193361cb7
Cheryl Toh <e0035693@u.nus.edu>
2019-04-05 01:25:42 +0800

    Validate form fields on client side in Register

--- blockchain_frontend/src/Register.js
+++ blockchain_frontend/src/Register.js
@@ -67,6 +67,166 @@ class Register extends Component {
     })
   }
 
+  handleEmailChange = event => {
+    this.setState( { email: event.target.value }, () => {
+      this.validateEmail();
+    });
+  }
+
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value }, () => {
+      this.validatePassword();
+    });
+  }
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value }, () => {
+      this.validateName();
+    });
+  }
+
+  handleHouseChange = event => {
+    this.setState({ house: event.target.value }, () => {
+      this.validateHouse();
+    });
+  }
+
+  handleStreetChange = event => {
+    this.setState({ street: event.target.value }, () => {
+      this.validateStreet();
+    });
+  }
+
+  handleCountryChange = event => {
+    this.setState({ country: event.target.value }, () => {
+      this.validateCountry();
+    });
+  }
+
+  handlePostalChange = event => {
+    this.setState({ postal: event.target.value }, () => {
+      this.validatePostal();
+    });
+  }
+
+  handleContactChange = event => {
+    this.setState({ contact: event.target.value }, () => {
+      this.validateContact();
+    });
+  }
+
+  validateEmail = () => {
+    const { email } = this.state;
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var errorMessage = '';
+    if (!email.match(regex)) {
+      errorMessage = 'Please enter a valid email';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
+  validatePassword = () => {
+    const { password } = this.state;
+    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+    var errorMessage = '';
+    if (!password.match(regex)) {
+      errorMessage = 'Password should contain at least 1 lowercase character, 1 uppercase character, 1 numeric character, and 1 special character';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
+  validateName = () => {
+    const { name } = this.state;
+    var regex = /^[a-zA-Z\s]+$/
+    var errorMessage = '';
+    if (!name.match(regex)) {
+      errorMessage = 'Name should only contain letters';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
+  validateHouse = () => {
+    const { house } = this.state;
+    var regex = /^[a-zA-Z0-9\s]+$/
+    var errorMessage = '';
+    if (!house.match(regex)) {
+      errorMessage = 'House should only contain letters and numbers';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
+  validateStreet = () => {
+    const { street } = this.state;
+    var regex = /^[a-zA-Z0-9\s]+$/
+    var errorMessage = '';
+    if (!street.match(regex)) {
+      errorMessage = 'Street should only contain letters and numbers';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
+  validateCountry = () => {
+    const { country } = this.state;
+    var regex = /^[a-zA-Z\s]+$/
+    var errorMessage = '';
+    if (!country.match(regex)) {
+      errorMessage = 'Country name should contain only letters';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
+  validatePostal = () => {
+    const { postal } = this.state;
+    var regex = /^[a-zA-Z0-9]+$/
+    var errorMessage = '';
+    if (!postal.match(regex)) {
+      errorMessage = 'Postal code should contain only letters and numbers';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
+  validateContact = () => {
+    const { contact } = this.state;
+    var regex = /^[0-9]+$/
+    var errorMessage = '';
+    if (!contact.match(regex)) {
+      errorMessage = 'Contact number should contain only numbers';
+    } else {
+      errorMessage = '';
+    }
+    this.setState({
+      infoMessage: errorMessage
+    });
+  }
+
   render () {
     return (
       <React.Fragment>
@@ -75,49 +235,49 @@ class Register extends Component {
           <p>Email:</p>
           <input type='text'
             id='email'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handleEmailChange}
             className='input-field' />
 
           <p>Password:</p>
-          <input type='text'
+          <input type='password'
             id='password'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handlePasswordChange}
             className='input-field' />
 
           <p>Name:</p>
           <input type='text'
             id='name'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handleNameChange}
             className='input-field' />
 
           <p>House:</p>
           <input type='text'
             id='house'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handleHouseChange}
             className='input-field' />
           
           <p>Street:</p>
           <input type='text'
             id='street'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handleStreetChange}
             className='input-field' />
           
           <p>Country:</p>
           <input type='text'
             id='country'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handleCountryChange}
             className='input-field' />
           
           <p>Postal Code:</p>
           <input type='text'
             id='postal'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handlePostalChange}
             className='input-field' />
 
           <p>Contact:</p>
           <input type='text'
             id='contact'
-            onChange={this.fieldChangeHandler}
+            onChange={this.handleContactChange}
             className='input-field' />
 
           <button type='submit' className="btn-primary btn-submit">
